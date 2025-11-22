fa3cac37df282d48805b090869282550bc585b2c
Cloudy <sankeerthpatnam101@gmail.com>
2025-11-22 12:38:41 -0500

    made updates

--- VSCodeProjects/Helios/src/App.jsx
+++ VSCodeProjects/Helios/src/App.jsx
@@ -1,18 +1,20 @@
-import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-
-import Home from "./pages/Home";
-import Foundry from "./pages/Foundry";
-import Projects from "./pages/Projects";
-import TurtleBeta from "./pages/TurtleBeta";
-import PelicanBeta from "./pages/PelicanBeta";
-import BelugaBeta from "./pages/BelugaBeta";
-import BeaverBeta from "./pages/BeaverBeta";
-import JoinOurMission from "./pages/JoinOurMission";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Foundry from './pages/Foundry.jsx';
+import Projects from './pages/Projects.jsx';
+import TurtleBeta from './pages/TurtleBeta.jsx';
+import PelicanBeta from './pages/PelicanBeta.jsx';
+import BelugaBeta from './pages/BelugaBeta.jsx';
+import BeaverBeta from './pages/BeaverBeta.jsx';
+import JoinOurMission from './pages/JoinOurMission.jsx';
+import NavBar from './components/NavBar.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
-    <Layout>
+    <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/foundry" element={<Foundry />} />
@@ -23,7 +25,8 @@ function App() {
         <Route path="/beaver-beta" element={<BeaverBeta />} />
         <Route path="/join-our-mission" element={<JoinOurMission />} />
       </Routes>
-    </Layout>
+      <Footer />
+    </>
   );
 }
 
